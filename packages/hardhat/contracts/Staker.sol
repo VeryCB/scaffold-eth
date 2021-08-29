6de3b9df4778bded04ee4a35d57e7cd93f7dac58
VeryCB <imcaibin@gmail.com>
2021-08-30 10:59:06 +1200

    Staker contract

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -7,24 +7,58 @@ contract Staker {
 
   ExampleExternalContract public exampleExternalContract;
 
+  mapping(address => uint) public balances;
+
+  uint public deadline;
+  uint public threshold = 1 ether;
+  bool public openForWithdraw = false;
+
+  event Stake(address indexed sender, uint value);
+
   constructor(address exampleExternalContractAddress) public {
+    deadline = block.timestamp + 30 seconds;
     exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
   }
 
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
   //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
+  function stake() public payable {
+    require(!exampleExternalContract.completed, 'Failed to stake: contract already completed.')
 
+    balances[msg.sender] += msg.value;
+    emit Stake(msg.sender, msg.value);
+  }
 
-  // After some `deadline` allow anyone to call an `execute()` function
-  //  It should either call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
+  function execute() public {
+    require(block.timestamp >= deadline, 'Cannot execute until deadline is reached.');
+    require(!exampleExternalContract.completed, 'Failed to execute: contract already completed.')
 
+    if (address(this).balance >= threshold) {
+      exampleExternalContract.complete{value: address(this).balance}();
+      // TODO: clear balance mapping
+    } else {
+      openForWithdraw = true;
+    }
+  }
 
+  function withdraw(address payable sender) public {
+    require(msg.sender == sender, 'Only the owner can withdraw.');
+    require(openForWithdraw, 'Withdraw is disabled.');
 
-  // if the `threshold` was not met, allow everyone to call a `withdraw()` function
+    uint balance = balances[sender];
+    require(balance > 0, 'Nothing to withdraw.');
 
+    bool sent = sender.send(balance);
+    require(sent, 'Failed to withdraw.');
 
+    delete balances[sender];
+  }
 
   // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
-
-
+  function timeLeft() public view returns (uint256) {
+    if (block.timestamp >= deadline) {
+      return 0;
+    }
+    return deadline - block.timestamp;
+  }
 }
